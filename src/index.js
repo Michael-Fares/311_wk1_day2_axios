1b5a71c8339eb7b532db70acf6cda3ddc0553bf9
Michael-Fares <mjfares@gmail.com>
2021-06-28 12:22:05 -0500

    axios practice tests passed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // import axios here
 
+
+
+const axios = require('axios')
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
@@ -8,17 +11,34 @@ const api_key = 'd771b19ef336ed8381def3a60b574464'
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  return axios(url)
+  .then(function (response) {
+    console.log('RESPONSE:',response);
+    return response;
+  });
 }
+
 
 const getMovieById = (id) => {
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
-  // code here
+  return axios(url)
+  .then(function(result){
+    console.log('MOVIE TITLE', result)
+    return result.data
+  })
 }
 
 const getMovieByIdFailure = () => {
   const fakeId = 1 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
-  // code here
+  return axios(url)
+  .then(function(response){
+    console.log('MOVIE TITLE DOES NOT EXIT', response)
+    return response.status
+  })
+  .catch(function (error) {
+    return error.response.status
+  })
 }
 
 
